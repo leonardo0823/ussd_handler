8f0de37ebed4900567e46c14948319ae41176452
leonardo0823 <mrleonardo0823@gmail.com>
2026-08-18 21:37:17 -0400

    feat: add initial implementation of UssdHandlerPlugin and related files

---
+++ ios/FlutterFramework/Package.swift
@@ -0,0 +1,7 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "FlutterFramework",
+    products: [.library(name: "FlutterFramework", targets: ["FlutterFramework"])],
+    targets: [.target(name: "FlutterFramework")])
